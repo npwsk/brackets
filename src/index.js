e0da4f9253b3d3c0f8165dbc5861cf36cfd04f6f
Nika Perfilova <ees2009.2014@gmail.com>
2022-01-24 10:03:39 +0700

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const sameBrackets = new Set(
+    bracketsConfig.filter(([opening, closing]) => opening === closing).map(([bracket]) => bracket)
+  );
+
+  const differentBrackets = bracketsConfig.filter(([opening, closing]) => opening !== closing);
+
+  const openingBrackets = new Set(differentBrackets.map(([opening]) => opening));
+  const openingToClosing = differentBrackets.reduce(
+    (acc, [opening, closing]) => ({ ...acc, [opening]: closing }),
+    {}
+  );
+
+  const stack = [];
+  for (const current of str) {
+    if (sameBrackets.has(current)) {
+      if (stack.length === 0 || stack[stack.length - 1] !== current) {
+        stack.push(current);
+        continue;
+      }
+      stack.pop();
+      continue;
+    }
+
+    if (openingBrackets.has(current)) {
+      stack.push(current);
+      continue;
+    }
+
+    const latest = stack.pop();
+    if (openingToClosing[latest] !== current) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
